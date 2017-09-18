6faa36c6fd1cbd5b9645d6ca5439bf29ee9352b0
Aidan <Aidan@Aidan-PC>
2017-09-17 20:02:50 -0700

    Teacher Searching is working completely

--- src/dataSort.java
+++ src/dataSort.java
@@ -3,55 +3,55 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class dataSort {
-	public static void SearchStudent(ArrayList<Student> students, String lastName) {
-		for (Student student: students) {
-			if (student.getStLastName().equals(lastName)) {
-				System.out.println(student.getStLastName() + ", " + student.getStFirstName() + ", " 
-						+ student.getGrade() + ", " + student.getClassroom() 
-						+ "," + student.gettLastName() + ", " + student.gettFirstName());
-			}
-		}
-	}
-	
+
 	public static boolean CheckFormat(String command) {
-		if ((!(command.charAt(1)==':')) || command.length() < 4 || !(command.charAt(2)== ' ')) {
+		if ((!(command.charAt(1) == ':')) || command.length() < 4 || !(command.charAt(2) == ' ')) {
 			return false;
-		}
-		else {
+		} else {
 			return true;
 		}
 	}
-	public static void SearchStudentBus(ArrayList<Student> students, String key) {
-		for (Student student: students) {
-			if (student.getStLastName().equals(key)) {
-				System.out.println(student.getStLastName() + ", " + student.getStFirstName() + ", " 
-						+ student.getBus());
+
+	public static void SearchStudent(ArrayList<Student> students, String lastName) {
+		for (Student student : students) {
+			if (student.getStLastName().equals(lastName)) {
+				System.out.println(student.getStLastName() + ", " + student.getStFirstName() + ", " + student.getGrade()
+						+ ", " + student.getClassroom() + ", " + student.gettLastName() + ", "
+						+ student.gettFirstName());
 			}
 		}
 	}
+
+	public static void SearchStudentBus(ArrayList<Student> students, String key) {
+		for (Student student : students) {
+			if (student.getStLastName().equals(key)) {
+				System.out.println(student.getStLastName() + ", " + student.getStFirstName() + ", " + student.getBus());
+			}
+		}
+	}
+
+	public static void SearchTeacher(ArrayList<Student> students, String key) {
+		for (Student student : students) {
+			if (student.gettLastName().equals(key)) {
+				System.out.println(student.getStLastName() + ", " + student.getStFirstName());
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner sc = new Scanner(System.in);
 		Scanner fs = new Scanner(new File("students.txt"));
-	
-		ArrayList<Student> students  = new ArrayList();
-		
-		String fullCommand;
-		char action;
-		String key;
-		
-		String stLastName;
-		String stFirstName;
-		int grade;
-		int classroom;
-		int bus;
+
+		ArrayList<Student> students = new ArrayList();
+
+		String fullCommand, key, stLastName, stFirstName, tLastName, tFirstName;
+		int grade, classroom, bus;
 		double gpa;
-		String tLastName;
-		String tFirstName;
-		
+		char action;
+
 		while (fs.hasNextLine()) {
-			
+
 			Scanner fs2 = new Scanner(fs.nextLine()).useDelimiter(",");
 			stLastName = fs2.next();
 			stFirstName = fs2.next();
@@ -61,10 +61,11 @@ public class dataSort {
 			gpa = fs2.nextDouble();
 			tLastName = fs2.next();
 			tFirstName = fs2.next();
-			
-			Student tempStudent = new Student(stLastName, stFirstName, grade, classroom, bus, gpa, tLastName, tFirstName);
+
+			Student tempStudent = new Student(stLastName, stFirstName, grade, classroom, bus, gpa, tLastName,
+					tFirstName);
 			tempStudent.print();
-			
+
 			students.add(tempStudent);
 		}
 
@@ -72,12 +73,12 @@ public class dataSort {
 			System.out.println("Enter next search");
 			fullCommand = sc.nextLine();
 			action = fullCommand.charAt(0);
-			if(fullCommand.length() > 0) {
-				switch(action) {
+			if (fullCommand.length() > 0) {
+				switch (action) {
 				case 'S':
 					if (CheckFormat(fullCommand)) {
-						if (fullCommand.charAt(fullCommand.length()-1) == 'B') {
-							key = fullCommand.substring(3,fullCommand.length() -2);
+						if (fullCommand.charAt(fullCommand.length() - 1) == 'B') {
+							key = fullCommand.substring(3, fullCommand.length() - 2);
 							SearchStudentBus(students, key);
 						}
 						key = fullCommand.substring(3);
@@ -85,6 +86,10 @@ public class dataSort {
 					}
 					break;
 				case 'T':
+					if (CheckFormat(fullCommand)) {
+						key = fullCommand.substring(3);
+						SearchTeacher(students, key);
+					}
 					break;
 				case 'B':
 					break;
@@ -98,9 +103,11 @@ public class dataSort {
 					break;
 				default:
 					System.out.println("Invalid Command...");
-				break;
+					break;
 				}
 			}
 		}
 	}
+
+	
 }
